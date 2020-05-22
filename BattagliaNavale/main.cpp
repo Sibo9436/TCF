226e9be05fb986971cc8da3a1bd1bf55d36f0270
Sibo9436 <sibo9436@gmail.com>
2020-05-22 15:59:01 +0200

    Aggiunge regole a inizio partita e lunghezza delle navi in fase di creazione

--- BattagliaNavale/main.cpp
+++ BattagliaNavale/main.cpp
@@ -25,6 +25,20 @@ int main()
   std::cout << """\n\n\t\t\t\t \033[43;1;5m Buona Partita \033[0m""" << std::endl;
   std::cout << std::string(25,'\n'); //"aggiorna" schermo
 
+
+  std::cout << "Conosci già le regole del gioco? (s/n): ";
+  char rulez;
+  std::cin >> rulez;
+  std::cin.ignore(10000,'\n');
+  if (rulez == 'n')
+  {
+    std::cout << "A inizio partita ad entrambi i giocatori sarà chiesto di posizionare la propia flotta sulla plancia di gioco, le restrizioni riguardanti il posizionamento della nave sono le seguenti:" << '\n';
+    std::cout << "\t ·le navi possono essere disposte solo verticalmente o orizzontalmente;"<< '\n';
+    std::cout << "\t ·le navi devono essere interamente incluse nella plancia di gioco;" << '\n';
+    std::cout << "\t ·le navi non possono sovrapporsi o toccarsi." << '\n';
+    std::cout << "Una volta terminata la prima fase, inizia la partita vera e propria. Lo scopo di ogni giocatore sarà quello di affondare la flotta dell'avversario, si avrà a disposizione un proiettile per turno, corrispondente alla dichiarazione del quadrante che si desidera colpire." << '\n';
+    std::cout << "Sono inoltre presenti due(tre) differenti modalità di gioco, in locale, contro un amico o contro la CPU, o in condivisione su rete locale, quest'ultima richiede che un giocatore assuma il ruolo di client e uno di server.\n\n\n";
+  }
   do {
     std::cout << "Per giocare su questo computer\t\t->\t \033[43;1;5m PREMI 1 \033[0m \nPer giocare online\t\t\t->\t \033[43;1;5m PREMI 2 \033[0m" << '\n';
     std::cin >> scelta;
